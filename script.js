46adc7763d2b4c5b1998828a0ddad45808d1fab6
Anderson Leite <92588198+AndersonLeite20@users.noreply.github.com>
2023-06-21 10:26:11 -0300

    funcionalidades
    já é possível fazer calculo usando o teclado numérico  e o resultado aparece em cima.

--- script.js
+++ script.js
@@ -1,22 +1,21 @@
+const divInj = document.getElementById('injetora')
 const  inputValor = document.getElementById('input')
 const inputAltura = document.getElementById('aut')
 const inputPeso = document.getElementById('pes')
 
 const caracteresPermitidos = ['0','1','2','3','4','5','6','7','8','9','.']
 
-document.querySelectorAll('buttonBtn').forEach(function(buttonBtnTodos){
-    buttonBtnTodos.addEventListener('click',function(){
-        const valor = buttonBtnTodos.dataset.value
-        inputAltura += valor
-        console.log(inputAltura)
-    })
-})
 inputAltura.addEventListener('keydown',function(ev){
     ev.preventDefault()
     if(caracteresPermitidos.includes(ev.key)){
         inputAltura.value += ev.key
-        console.log(inputAltura.value)
         return
+    }
+    if(ev.key === 'Backspace'){
+        inputAltura.value = inputAltura.value.slice(0,-1)
+    }
+    if(ev.key === 'Enter'){
+      //  calcular()
     }
 })
 inputPeso.addEventListener('keydown',function(ev){
@@ -25,9 +24,26 @@ inputPeso.addEventListener('keydown',function(ev){
         inputPeso.value += ev.key
         return
     }
-    if()
+    if(ev.key === 'Backspace'){
+        inputPeso.value = inputPeso.value.slice(0,-1)
+    }
+    if(ev.key === 'Enter'){
+        calcular()
+    }
 })
+function calcular(){
+    let altura = parseFloat(inputAltura.value)
+    let altoQuad = Math.pow(altura,altura)
+    let peso = parseFloat(inputPeso.value)
+    const resultado = peso/altoQuad
+    console.log(resultado)
+    const h2 = document.createElement('h2')
+    h2.innerText = resultado.toFixed(2)
+    h2.className = 'h2Result'
+    divInj.append(h2)
+}
 document.getElementById('limpar').addEventListener('click',function(){
     document.getElementById('aut').value = ''
     document.getElementById('pes').value = ''
+    document.querySelector('.h2Result').remove()
 })
